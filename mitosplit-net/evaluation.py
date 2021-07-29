2079f2f2f055527d091ced67c7b568955b637267
santi-rodriguez <rodriguezsantiago96@gmail.com>
2021-07-29 15:33:25 +0200

    Calculate metrics from confusion matrix instead of tk.keras.metrics

--- mitosplit-net/evaluation.py
+++ mitosplit-net/evaluation.py
@@ -1,7 +1,5 @@
 import numpy as np
 from tqdm import tqdm
-
-
 import tensorflow as tf
 
 def predict(input_test, model):
@@ -14,19 +12,60 @@ def predict(input_test, model):
             pred_output_test[model_id] = model[model_name].predict(input_test)[:, :, :, 0]
         return pred_output_test
       
-def get_metrics(y_true, y_pred, thresholds):
-  metrics = {'binary accuracy': tf.metrics.BinaryAccuracy(),
-             'precision': tf.metrics.Precision(),
-             'recall': tf.metrics.Recall()}
-  nb_thresholds = len(thresholds)
-  metrics_test = {metric_name: np.zeros(nb_thresholds) for metric_name in metrics}
-  y_true_binary = y_true>0
-  for i, thr in tqdm(enumerate(thresholds), total=nb_thresholds):
-    y_pred_binary = y_pred>=thr
-    for metric_name in metrics:
-      metrics[metric_name].update_state(y_true_binary, y_pred_binary)
-      metrics_test[metric_name][i] = metrics[metric_name].result().numpy()
-  return metrics_test
+def confusion_matrix(outputs, predictions, threshold):
+  out_binary = outputs>0
+  nb_pixels = out_binary.size
+
+  if isinstance(threshold, (int, float)):
+    conf_matrix = np.zeros((2, 2))
+    pred_binary = predictions>threshold
+    
+    tp_mask = out_binary & pred_binary
+    fn_mask = out_binary & (~pred_binary)
+    fp_mask = (~out_binary) & pred_binary
+    
+    conf_matrix[0, 0] = tp_mask.sum() #True Positives
+    conf_matrix[0, 1] = fn_mask.sum() #False Negatives
+    conf_matrix[1, 0] = fp_mask.sum() #False Positives
+    conf_matrix[1, 1] = nb_pixels - conf_matrix[0, 0] - conf_matrix[0, 1] - conf_matrix[1, 0] #True Negatives
+    return conf_matrix.astype(int)
+  
+  nb_thr = len(threshold)
+  conf_matrix = np.zeros((nb_thr, 2, 2))
+  for i, thr in tqdm(enumerate(threshold), total=nb_thr):
+    pred_binary = predictions>thr
+    
+    tp_mask = out_binary & pred_binary
+    fn_mask = out_binary & (~pred_binary)
+    fp_mask = (~out_binary) & pred_binary
+    
+    conf_matrix[i, 0, 0] = tp_mask.sum() #True Positives
+    conf_matrix[i, 0, 1] = fn_mask.sum() #False Negatives
+    conf_matrix[i, 1, 0] = fp_mask.sum() #False Positives
+    conf_matrix[i, 1, 1] = nb_pixels - conf_matrix[i, 0, 0] - conf_matrix[i, 0, 1] - conf_matrix[i, 1, 0] #True Negatives
+    
+  return conf_matrix.astype(int)
+
+def get_metrics(outputs, predictions, threshold):
+    conf_matrix = confusion_matrix(outputs, predictions, threshold)
+    
+    if isinstance(threshold, (int, float)):
+        tp = conf_matrix[0, 0]
+        fn = conf_matrix[0, 1]
+        fp = conf_matrix[1, 0]
+        tn = conf_matrix[1, 1]
+    else:
+        tp = conf_matrix[:, 0, 0]
+        fn = conf_matrix[:, 0, 1]
+        fp = conf_matrix[:, 1, 0]
+        tn = conf_matrix[:, 1, 1]
+    
+    metrics = {'binary accuracy': (tp+tn)/(tp+tn+fp+fn), 
+               'precision': tp/(tp+fp), 
+               'TPR': tp/(tp+fn),
+               'FPR': fp/(fp+tn)}
+    metrics['F1-score'] = 2*(metrics['precision']*metrics['TPR'])/(metrics['precision']+metrics['TPR'])
+    return metrics
 
 def detection_match(y_true, y_pred, threshold=0.99):
     total = y_true.shape[0]
