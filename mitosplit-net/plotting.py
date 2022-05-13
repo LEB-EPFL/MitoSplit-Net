88e82b5849b0405216142cdc566b3dfe1affd5af
santi-rodriguez <rodriguezsantiago96@gmail.com>
2022-05-13 10:18:19 +0200

    plot_merge: don't turn axis off

--- mitosplit-net/plotting.py
+++ mitosplit-net/plotting.py
@@ -23,7 +23,6 @@ def plot_merge(input_img, output_img, cmap=None, title='Mito + GT',
   ax.imshow(input_img, cmap=cmap[0])
   ax.imshow(output_img, cmap=cmap[1], alpha=alpha)
   ax.set(xticks=[], yticks=[])
-  ax.axis('off')
   return ax
   
 def plot_comparison(input_img, output_img, cmap = ['gray', 'inferno'],
